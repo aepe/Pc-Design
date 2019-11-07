7cc7095ee05685f8d2dbd3f1a3a029093c7703e0
wangxiaolong <xiaolong1498@gmail.com>
2019-11-07 11:33:27 +0800

    新增tree组件

--- vuepress/.vuepress/config/themeConfig.js
+++ vuepress/.vuepress/config/themeConfig.js
@@ -1,6 +1,6 @@
 /*
- * @abstract: 
- * @version: 
+ * @abstract:
+ * @version:
  * @Author: bhabgs
  * @Date: 2019-11-07 09:48:14
  * @LastEditors: bhabgs
@@ -9,21 +9,18 @@
 const themeConfig = {
     nav: [
         { text: '指南', link: '/' },
-        { text: '组件',
+        {
+            text: '组件',
             items: [
                 // { text: 'react', link: '/react/' },
                 { text: 'vue', link: '/vue/' },
                 // { text: 'ts', link: '/ts/' }
-            ]
+            ],
         },
         { text: '主题', link: '/vue/theme/' },
     ],
     sidebar: {
-        '/vue/': [
-            '',
-            'button/',
-            'theme/'
-        ]
-    }
-}
+        '/vue/': ['', 'button/', 'tree/', 'theme/'],
+    },
+};
 exports.themeConfig = themeConfig;
