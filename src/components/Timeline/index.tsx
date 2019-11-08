e305f2ed16f10d35197ccedd5b2d21a96996ee7e
wangxiaolong <xiaolong1498@gmail.com>
2019-11-08 10:40:11 +0800

    timeline component

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -1,14 +1,31 @@
 import { Component, Vue, Prop } from 'vue-property-decorator';
 
 @Component
-export default class MtTimeline extends Vue {
+export default class ZTimeline extends Vue {
     // @Prop(String)
     // private propA: string;
-    msg: string = 'This is mt timeline component';
+    msg: string = 'This is z timeline component';
     render(): any {
         return (
-            <div class="mt-timeline">
-                <h1>{this.msg}</h1>
+            <div class="z-timeline">
+                <div class="z-timeline-inner">
+                    <div class="z-timeline-item">
+                        <div class="z-timeline-line"></div>
+                        <div class="z-timeline-dot"></div>
+                        <div class="z-timeline-content">
+                            <div class="z-timeline-title">我是标题</div>
+                            <div class="z-timeline-summary">我是内容</div>
+                        </div>
+                    </div>
+                    <div class="z-timeline-item">
+                        <div class="z-timeline-line"></div>
+                        <div class="z-timeline-dot"></div>
+                        <div class="z-timeline-content">
+                            <div class="z-timeline-title">我是标题</div>
+                            <div class="z-timeline-summary">我是内容</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
