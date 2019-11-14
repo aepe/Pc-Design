5c78a7379821c889b6e23a8e918739c111364f65
langxue <641078926@qq.com>
2019-11-14 17:13:59 +0800

    input组件 -  限制输入长度示例组件添加

---
+++ docs/.vuepress/components/Input/demo-length.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: 
+ * @Date: 2019-11-14 16:03:05
+ * @LastEditors: langxue
+ * @LastEditTime: 2019-11-14 17:07:31
+ -->
+<template>
+  <div id="input-demo-length">
+    <z-input type="text"
+          placeholder="请输入内容"
+          :max="5"
+          :min="2" />
+  </div>
+</template>
+<style lang="stylus">
+#input-demo-length {
+  width 180px;
+}
+</style>
