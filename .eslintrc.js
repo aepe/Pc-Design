16824c16f4f131ef39bc38359dc95aac45bd292b
bhabgs <bhabgs@aliyun.com>
2019-11-14 10:53:09 +0800

    更新依赖关系

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,21 @@
  * @Author: bhabgs
  * @Date: 2019-11-13 16:46:16
  * @LastEditors: bhabgs
- * @LastEditTime: 2019-11-14 10:29:05
+ * @LastEditTime: 2019-11-14 10:50:43
  */
 module.exports = {
     parser: '@typescript-eslint/parser',
     // extends: "standard",
     env: {
         "browser": true
+    },
+    parserOptions: {
+        ecmaVersion: 6, //也就是ES6语法支持的意思
+        sourceType: 'module',
+        ecmaFeatures: {
+            modules: true,
+        },
+        project: './tsconfig.json',
     },
     rules: {
         "semi": ["error", "always"],
