e27def4aaf738167bf4fcffd2f689d08d7fa3a5a
bhabgs <bhabgs@aliyun.com>
2019-12-12 11:13:49 +0800

    优化icon文档

--- docs/.vuepress/components/Icon/index.vue
+++ docs/.vuepress/components/Icon/index.vue
@@ -4,14 +4,17 @@
  * @Author: bhabgs
  * @Date: 2019-11-12 11:07:52
  * @LastEditors: bhabgs
- * @LastEditTime: 2019-12-11 19:01:41
+ * @LastEditTime: 2019-12-12 09:28:58
  -->
 <template>
   <div class="bhabgs-block">
     Icon:
-    <div class="icon-box" v-for="i in icons">
-      <ZIcon :type="i" />
+    <div class="icons">
+      <div class="icon-box" v-for="i in icons" @click="changeIcon(i)">
+        <ZIcon :type="i" />
+      </div>
     </div>
+    <textarea v-model="icon" cols="30" rows="10" id="textarea"></textarea>
   </div>
 </template>
 <script>
@@ -30,10 +33,20 @@ export default {
         "info",
         "sousuo",
         "close"
-      ]
+      ],
+      icon: ""
     };
   },
-  methods: {}
+  methods: {
+    changeIcon(item) {
+      this.icon = `<ZIcon type="${item}"/>`;
+      this.$nextTick(() => {
+        const text = document.getElementById("textarea");
+        text.select();
+        document.execCommand("Copy");
+      });
+    }
+  }
 };
 </script>
 <style lang="stylus" scoped>
@@ -45,12 +58,44 @@ export default {
   margin-bottom: 1rem;
 }
 
-.icon-box {
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  margin: 9px 8px;
-  height: 137px;
-  position: relative;
+textarea {
+  width: 1px;
+  height: 1px;
+  left: -100px;
+  position: absolute;
+}
+
+.icons {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+
+  .icon-box {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    margin: 9px 8px;
+    height: 137px;
+    position: relative;
+
+    i {
+      font-size: 2rem;
+    }
+
+    &:before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      transition: all 0.3s;
+      right: 0;
+      bottom: 0;
+    }
+
+    &:hover {
+      &:before {
+        background-color: rgba(0, 0, 0, 0.3);
+      }
+    }
+  }
 }
 </style>
