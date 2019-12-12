849516490a3589ed0b7cfbf0f870a144e5fb97fc
bhabgs <bhabgs@aliyun.com>
2019-12-12 11:12:23 +0800

    添加icon组件

--- src/components/Icon/icon.tsx
+++ src/components/Icon/icon.tsx
@@ -1,4 +1,4 @@
-import { Vue, Component, Prop } from "vue-property-decorator";
+import { Vue, Component, Prop, Emit } from "vue-property-decorator";
 
 @Component
 export default class Icon extends Vue {
@@ -7,12 +7,23 @@ export default class Icon extends Vue {
     default: ""
   })
   private type!: string;
+
+  @Emit("click")
+  public btnClick(e: Event) {
+    return e;
+  }
   private get classes(): string {
     let className: string = "iconfont zxicon zx";
     return className + (this.type || "-loading");
   }
   render(): JSX.Element {
-    const { classes, $slots } = this;
-    return <i class={classes}>{$slots.default}</i>;
+    const { classes, $slots, btnClick } = this;
+    const iconProps = {
+      class: classes,
+      on: {
+        click: btnClick
+      }
+    };
+    return <i {...iconProps}>{$slots.default}</i>;
   }
 }
